909d2b51b5d9d9bfb932683abe6aade57146a858
Никита Альбрандт <4xplor4r@gmail.com>
2025-12-02 04:37:26 +0300

    Added a new method for calculating the area of a triangle.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
+import java.lang.Math;
+
 class Main {
     public static double calculateRectangleSquare(double a, double b) {
         double s = (a + a) + (b + b);
+        return s;
+    }
+
+    public static double calculateTriangleSquare(double a, double b) {
+        double s = 1.0/2 * a * b * Math.sin(Math.toRadians(90));
         return s;
     }
 
@@ -8,5 +15,6 @@ class Main {
         System.out.println("Это добавлено в ветке master");
 
         System.out.println(calculateRectangleSquare(2.1, 4.5));
+        System.out.println(calculateTriangleSquare(3.0, 4.0));
     }
 }
