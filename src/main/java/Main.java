46bf1cc66edc0ced65894639f51a4c898f4b5b66
Никита Альбрандт <4xplor4r@gmail.com>
2025-12-02 04:55:54 +0300

    A new method for calculating the area of a trapezoid has been added.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,10 +11,16 @@ class Main {
         return s;
     }
 
+    public static double calculateTrapezoidSquare(double a, double b, double h) {
+        double s = (a + b) / 2 * h;
+        return s;
+    }
+
     public static void main(String[] args) {
         System.out.println("Это добавлено в ветке master");
 
         System.out.println(calculateRectangleSquare(2.1, 4.5));
         System.out.println(calculateTriangleSquare(3.0, 4.0));
+        System.out.println(calculateTrapezoidSquare(7.1, 8.4, 2.0));
     }
 }
