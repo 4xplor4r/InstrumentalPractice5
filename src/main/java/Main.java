eda874d07858bc92b3ad45d08689c67070ef5420
Никита Альбрандт <4xplor4r@gmail.com>
2025-12-02 04:28:03 +0300

    Added a new method for calculating the area of a rectangle.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
 class Main {
+    public static double calculateRectangleSquare(double a, double b) {
+        double s = (a + a) + (b + b);
+        return s;
+    }
+
     public static void main(String[] args) {
         System.out.println("Это добавлено в ветке master");
+
+        System.out.println(calculateRectangleSquare(2.1, 4.5));
     }
 }
